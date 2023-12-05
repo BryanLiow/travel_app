e4fdde0174e5a40ceb1e2c40c88b922939ea3228
callMeDrStrange <bryan.liow.zy@gmail.com>
2023-12-05 01:40:21 +0000

    Update content card

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,15 +1,18 @@
 "use client";
 import * as React from "react";
 import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import Link from "next/link";
 import usePost from "./contexts/PostContext";
 import { useRouter } from "next/navigation";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 type ContentCardProps = {
   randomHeight: string;
@@ -51,9 +54,21 @@ const ContentCard = ({
   return (
     <a onClick={handleCardClick} className="hover:cursor-pointer">
       <Card
-        className="rounded-xl border-solid border-1 border-gray-900"
+        className="rounded-xl border-solid border-2 border-gray-100"
         sx={{ maxWidth: 350 }}
       >
+        {randomHeight == "true" && (
+          <CardHeader
+            avatar={<Avatar aria-label="recipe">R</Avatar>}
+            title="user name"
+            subheader={
+              <div className="text-xs text-gray-300">
+                <span>{location}</span>
+              </div>
+            }
+          />
+        )}
+
         <CardMedia
           className={
             randomHeight === "true"
@@ -64,24 +79,16 @@ const ContentCard = ({
           image={thumbnail}
           alt=""
         />
-
         <CardContent>
           <Typography>{contentCardTitle}</Typography>
         </CardContent>
         <CardActions>
           <div className="grid grid-cols-2 w-full">
-            <div>
+            <div className="text-xs">
               <FavoriteIcon />
               <span className="text-xs">{likes}</span>
             </div>
-            <div className="text-xs text-right">
-              <LocationOnIcon />
-              <span>{location}</span>
-            </div>
-            <div className="text-xs text-gray-500">
-              <span>@{user}</span>
-            </div>
-            <div className="text-xs text-gray-500 text-right">
+            <div className="text-xs text-right pt-1">
               <span>{createdOn}</span>
             </div>
           </div>
